0588ff5c22d987659ded15dc4150d1848e82075b
priyansh-garg1 <priyanshgarg382@gmail.com>
2024-07-15 13:03:28 +0530

    Added API

--- app/create-trip/generateTrip.jsx
+++ app/create-trip/generateTrip.jsx
@@ -1,44 +1,104 @@
-import { View, Text, Image } from 'react-native'
-import React from 'react'
-import { Colors } from '../../constants/Colors'
+import { View, Text, Image } from "react-native";
+import React, { useContext, useEffect, useState } from "react";
+import { Colors } from "../../constants/Colors";
+import { useNavigation, useRouter } from "expo-router";
+import { CreateTripContext } from "../../context/CreateTripContext";
+import { AI_PROMPT } from "../../constants/Ooptions";
+import { chatSession } from "../../configs/AiModel";
+import { auth, db } from "../../configs/FirebaseConfig";
+import { doc, setDoc } from "firebase/firestore";
 
 export default function GenerateTrip() {
+  const navigation = useNavigation();
+  const router = useRouter();
+  const { tripData, setTripData } = useContext(CreateTripContext);
+  const [loading, setLoading] = useState(false);
+  const user = auth.currentUser;
+
+  useEffect(() => {
+    GenerateAiTrip();
+  }, []);
+
+  const GenerateAiTrip = async () => {
+    setLoading(true);
+    try {
+      const FINAL_PROMPT = AI_PROMPT.replace(
+        "{location}",
+        tripData?.locationInfo?.name
+      )
+        .replace("{totalDay}", tripData?.totalNoOfDays)
+        .replace("{totalNight}", tripData?.totalNoOfDays - 1)
+        .replace("{traveler}", tripData?.traveler?.title)
+        .replace("{budget}", tripData?.budget);
+
+      const result = await chatSession.sendMessage(FINAL_PROMPT);
+      console.log(result.response.text(), "line 35");
+      const tripResp = JSON.parse(result.response.text());
+      console.log(tripResp, "line 37");
+
+      const docId = Date.now().toString();
+      const result_ = await setDoc(doc(db, "UserTrips", docId), {
+        userEmail: user?.email,
+        tripPlan: tripResp,
+        tripData: JSON.stringify(tripData),
+        docId: docId
+      });
+      console.log(result_,"Line 45");
+    } catch (error) {
+      console.log("Erorr occurred while generating trip data");
+      console.log(error);
+    }
+    setLoading(false);
+    router.push("/(tabs)/mytrip");
+  };
+
   return (
-    <View style={{
-        padding:25,
-        marginTop:75,
-        backgroundColor:Colors.white,
-        height:'100%',
-    }}>
-      <Text style={{
-        fontSize:35,
-        fontFamily:'outfit-bold',
-        marginTop:20,
-        textAlign:'center',
-      }}>
+    <View
+      style={{
+        padding: 25,
+        marginTop: 75,
+        backgroundColor: Colors.white,
+        height: "100%",
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 35,
+          fontFamily: "outfit-bold",
+          marginTop: 20,
+          textAlign: "center",
+        }}
+      >
         Please Wait...
       </Text>
-      <Text style={{
-        fontSize:20,
-        fontFamily:'outfit-medium',
-        marginTop:40,
-        textAlign:'center',
-      }}>
-        We are working to generate your 
+      <Text
+        style={{
+          fontSize: 20,
+          fontFamily: "outfit-medium",
+          marginTop: 40,
+          textAlign: "center",
+        }}
+      >
+        We are working to generate your
       </Text>
-      <Image source={require('./../../assets/images/aeroplane.gif')}
-      style={{
-        width:'100%',
-        height:300,
-        objectFit:"contain"
-      }}
+      <Image
+        source={require("./../../assets/images/aeroplane.gif")}
+        style={{
+          width: "100%",
+          height: 200,
+          objectFit: "contain",
+        }}
       />
-      <Text style={{
-        fontSize:20,
-        fontFamily:'outfit',
-        color:Colors.GRAY,
-        textAlign:'center',
-      }}>Don't Go Back</Text>
+      <Text
+        style={{
+          fontSize: 20,
+          fontFamily: "outfit",
+          color: Colors.GRAY,
+          textAlign: "center",
+        }}
+      >
+        Don't Go Back
+      </Text>
     </View>
-  )
+  );
 }
--- constants/Ooptions.js
+++ constants/Ooptions.js
@@ -1,51 +1,54 @@
-export const SelectTravlersList =[
-    {
-        id:1,
-        title:"Just Me",
-        desc:"A sole travelers in exploration",
-        icon:"🛩",
-        pepole:"1"
-    },
-    {
-        id:2,
-        title:"A Couple",
-        desc:"Two travelers in tandem",
-        icon:"🥂",
-        pepole:"2"
-    },
-    {
-        id:3,
-        title:"Family",
-        desc:"A group of fun loving adventure",
-        icon:"🏡",
-        pepole:"3 to 5"
-    },
-    {
-        id:4,
-        title:"Freinds",
-        desc:"A bunch of thrill-seekes",
-        icon:"🏝",
-        pepole:"5-10"
-    },
-]
+export const SelectTravlersList = [
+  {
+    id: 1,
+    title: "Just Me",
+    desc: "A sole travelers in exploration",
+    icon: "🛩",
+    pepole: "1",
+  },
+  {
+    id: 2,
+    title: "A Couple",
+    desc: "Two travelers in tandem",
+    icon: "🥂",
+    pepole: "2",
+  },
+  {
+    id: 3,
+    title: "Family",
+    desc: "A group of fun loving adventure",
+    icon: "🏡",
+    pepole: "3 to 5",
+  },
+  {
+    id: 4,
+    title: "Freinds",
+    desc: "A bunch of thrill-seekes",
+    icon: "🏝",
+    pepole: "5-10",
+  },
+];
 
 export const SelectBudgetOptions = [
-    {
-        id:1,
-        title:"Cheap",
-        desc:"Stay conscious of costs",
-        icon:"💵",
-    },
-    {
-        id:2,
-        title:"Moderate",
-        desc:"Keep cost on the average side",
-        icon:"💰",
-    },
-    {
-        id:3,
-        title:"Luxury",
-        desc:"Don't worry about cost",
-        icon:"💸",
-    },
-]
+  {
+    id: 1,
+    title: "Cheap",
+    desc: "Stay conscious of costs",
+    icon: "💵",
+  },
+  {
+    id: 2,
+    title: "Moderate",
+    desc: "Keep cost on the average side",
+    icon: "💰",
+  },
+  {
+    id: 3,
+    title: "Luxury",
+    desc: "Don't worry about cost",
+    icon: "💸",
+  },
+];
+
+export const AI_PROMPT =
+  "Generate Travel Plan for Location : {location}, for {totalDay} Days and {totalNight} Night for {traveler} with a {budget} budget with a flight details , Flight Price with Booking url, Hotels options list with Hotel Name, Hotel Address, Price, Hotel Image url, geo coordinates, rating, descriptions and Places to visit nearby with placeName, Place Details, Place Image Url, Geo Coordinates, ticket pricing, Time to travel each of the location for {totalDay} days and {totalNight} night with each day plan with best time to visit in JSON format.";
